bca93ffae6e004dbddd30a6584439bd98c542244
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-11 12:58:57 +0530

    update home.ts

--- movie_project/src/app/app.module.ts
+++ movie_project/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { MovieComponent } from './movie/movie.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- movie_project/src/app/home/home.component.ts
+++ movie_project/src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  trendingMovies: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getTrendingMovies();
+  }
+
+  getTrendingMovies() {
+    this.http
+    .get('http://localhost:4200/assets/data/trending-movies.json')
+    .subscribe((movies) => {
+      this.trendingMovies = movies;
+      console.log(this.trendingMovies)
+    });
+  }
 
 }
