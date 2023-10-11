94cb46e91f7c2b42aaba074aac5018aa05002a14
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-10-11 12:25:23 +0530

    Update header functions

--- movie_project/src/app/header/header.component.ts
+++ movie_project/src/app/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  ngOnInit(): void {
+      
+  }
+
+  goToHome() {
+    this.router.navigate(['home']);
+  }
+
+  logout() {
+
+  }
 
 }
--- movie_project/src/app/login/login.component.ts
+++ movie_project/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -7,7 +7,7 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
   username = "";
   password = "";
   errorMsg = "";
